27a6d9f796b20bb6f9c45aaf842488e09824a5f0
Jasleen Sandhu <jasleensandhu187@gmail.com>
2024-07-19 11:58:00 -0700

    Fixed scene three html css

--- scene_three.js
+++ scene_three.js
@@ -21,41 +21,41 @@ Promise.all([
             if (match2017 && match2018 && match2019) {
                 merged.push({
                     "Country": d1["Country or region"],
-                    "Score_2016": +d1.Score,
-                    "Score_2017": +match2017.Score,
-                    "Score_2018": +match2018.Score,
-                    "Score_2019": +match2019.Score,
+                    "Score_2016": (+d1.Score).toFixed(2),
+                    "Score_2017": (+match2017.Score).toFixed(2),
+                    "Score_2018": (+match2018.Score).toFixed(2),
+                    "Score_2019": (+match2019.Score).toFixed(2),
                     "Factors_2016": {
-                        "GDP per capita": +d1["GDP per capita"],
-                        "Social support": +d1["Social support"],
-                        "Healthy life expectancy": +d1["Healthy life expectancy"],
-                        "Freedom to make life choices": +d1["Freedom to make life choices"],
-                        "Generosity": +d1["Generosity"],
-                        "Perceptions of corruption": +d1["Perceptions of corruption"]
+                        "GDP per capita": (+d1["GDP per capita"]).toFixed(2),
+                        "Social support": (+d1["Social support"]).toFixed(2),
+                        "Healthy life expectancy": (+d1["Healthy life expectancy"]).toFixed(2),
+                        "Freedom to make life choices": (+d1["Freedom to make life choices"]).toFixed(2),
+                        "Generosity": (+d1["Generosity"]).toFixed(2),
+                        "Perceptions of corruption": (+d1["Perceptions of corruption"]).toFixed(2)
                     },
                     "Factors_2017": {
-                        "GDP per capita": +match2017["GDP per capita"],
-                        "Social support": +match2017["Social support"],
-                        "Healthy life expectancy": +match2017["Healthy life expectancy"],
-                        "Freedom to make life choices": +match2017["Freedom to make life choices"],
-                        "Generosity": +match2017["Generosity"],
-                        "Perceptions of corruption": +match2017["Perceptions of corruption"]
+                        "GDP per capita": (+match2017["GDP per capita"]).toFixed(2),
+                        "Social support": (+match2017["Social support"]).toFixed(2),
+                        "Healthy life expectancy": (+match2017["Healthy life expectancy"]).toFixed(2),
+                        "Freedom to make life choices": (+match2017["Freedom to make life choices"]).toFixed(2),
+                        "Generosity": (+match2017["Generosity"]).toFixed(2),
+                        "Perceptions of corruption": (+match2017["Perceptions of corruption"]).toFixed(2)
                     },
                     "Factors_2018": {
-                        "GDP per capita": +match2018["GDP per capita"],
-                        "Social support": +match2018["Social support"],
-                        "Healthy life expectancy": +match2018["Healthy life expectancy"],
-                        "Freedom to make life choices": +match2018["Freedom to make life choices"],
-                        "Generosity": +match2018["Generosity"],
-                        "Perceptions of corruption": +match2018["Perceptions of corruption"]
+                        "GDP per capita": (+match2018["GDP per capita"]).toFixed(2),
+                        "Social support": (+match2018["Social support"]).toFixed(2),
+                        "Healthy life expectancy": (+match2018["Healthy life expectancy"]).toFixed(2),
+                        "Freedom to make life choices": (+match2018["Freedom to make life choices"]).toFixed(2),
+                        "Generosity": (+match2018["Generosity"]).toFixed(2),
+                        "Perceptions of corruption": (+match2018["Perceptions of corruption"]).toFixed(2)
                     },
                     "Factors_2019": {
-                        "GDP per capita": +match2019["GDP per capita"],
-                        "Social support": +match2019["Social support"],
-                        "Healthy life expectancy": +match2019["Healthy life expectancy"],
-                        "Freedom to make life choices": +match2019["Freedom to make life choices"],
-                        "Generosity": +match2019["Generosity"],
-                        "Perceptions of corruption": +match2019["Perceptions of corruption"]
+                        "GDP per capita": (+match2019["GDP per capita"]).toFixed(2),
+                        "Social support": (+match2019["Social support"]).toFixed(2),
+                        "Healthy life expectancy": (+match2019["Healthy life expectancy"]).toFixed(2),
+                        "Freedom to make life choices": (+match2019["Freedom to make life choices"]).toFixed(2),
+                        "Generosity": (+match2019["Generosity"]).toFixed(2),
+                        "Perceptions of corruption": (+match2019["Perceptions of corruption"]).toFixed(2)
                     }
                 });
             }
@@ -88,6 +88,7 @@ Promise.all([
         function updateFactorsChart(year, country) {
             var selectedData = mergedData.find(d => d.Country === country);
             var factors = selectedData["Factors_" + year];
+            var score = selectedData["Score_" + year];
 
             // Clear previous chart
             d3.select("#factors-chart").html("");
@@ -102,6 +103,15 @@ Promise.all([
                 .append("g")
                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+            // Add happiness score above the chart
+            svg.append("text")
+                .attr("x", (width / 2))
+                .attr("y", -20)
+                .attr("text-anchor", "middle")
+                .style("font-size", "16px")
+                .style("font-weight", "bold")
+                .text(`Happiness Score for ${country} in ${year}: ${score}`);
+
             var x = d3.scaleBand()
                 .range([0, width])
                 .padding(0.2) // Adjusted padding
@@ -109,7 +119,7 @@ Promise.all([
 
             var y = d3.scaleLinear()
                 .range([height, 0])
-                .domain([0, d3.max(Object.values(factors)) + 0.2]); // Adjusted domain for y-scale
+                .domain([0, d3.max(Object.values(factors).map(d => +d)) + 0.2]); // Adjusted domain for y-scale
             
             svg.append("g")
                 .attr("transform", "translate(0," + height + ")")
@@ -128,9 +138,9 @@ Promise.all([
                 .enter().append("rect")
                 .attr("class", "bar")
                 .attr("x", d => x(d[0]))
-                .attr("y", d => y(d[1]))
+                .attr("y", d => y(+d[1]))
                 .attr("width", x.bandwidth())
-                .attr("height", d => height - y(d[1]))
+                .attr("height", d => height - y(+d[1]))
                 .style("fill", "maroon");
 
             svg.selectAll(".bar-label")
@@ -138,17 +148,9 @@ Promise.all([
                 .enter().append("text")
                 .attr("class", "bar-label")
                 .attr("x", d => x(d[0]) + x.bandwidth() / 2)
-                .attr("y", d => y(d[1]) - 5)
+                .attr("y", d => y(+d[1]) - 5)
                 .attr("text-anchor", "middle")
-                .text(d => d[1]);
-
-            svg.append("text")
-                .attr("x", (width / 2))
-                .attr("y", 0 - (margin.top / 2))
-                .attr("text-anchor", "middle")
-                .style("font-size", "18px")
-                .style("text-decoration", "underline")
-                .text("Factors Contributing to Happiness Score in " + year + " for " + country);
+                .text(d => (+d[1]).toFixed(2));
         }
 
         // Initial call to update factors chart with default selections
